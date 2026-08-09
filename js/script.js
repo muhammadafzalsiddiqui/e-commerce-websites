03abb8c87b49981aefd15020d5eaa9e42f2b4c19
Muhammad Afzalmuhammadafzal.fullstack@gmail.comgit config --global user.email <muhammadafzal.fullstack@gmail.com>
2026-08-09 18:55:47 +0500

    product done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 function toggleMenu() {
   document.getElementById('navLinks').classList.toggle('active');
 }
+document.querySelectorAll('.card-img').forEach(card => {
+  let images = card.querySelectorAll('img');
+  let index = 0;
+  let interval;
+
+  // Default image active
+  images[0].classList.add('active');
+
+  card.addEventListener('mouseenter', () => {
+    interval = setInterval(() => {
+      images[index].classList.remove('active');
+      index = (index + 1) % images.length;
+      images[index].classList.add('active');
+    }, 1000); // 1 second delay
+  });
+
+  card.addEventListener('mouseleave', () => {
+    clearInterval(interval);
+    images.forEach(img => img.classList.remove('active'));
+    images[0].classList.add('active'); // reset to default
+  });
+});
